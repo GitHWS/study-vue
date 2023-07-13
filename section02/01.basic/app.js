091ec572c376160e750b65a6b4ec9c58e3374236
Hyunwoo Shin <96808980+GitHWS@users.noreply.github.com>
2023-07-13 22:38:58 +0900

    불필요한 주석 제거

--- section02/01.basic/app.js
+++ section02/01.basic/app.js
@@ -19,26 +19,3 @@ const app = Vue.createApp({
   },
 });
 app.mount('#user-goal');
-
-// 내 예제
-// const app = Vue.createApp({
-//   data() {
-//     return {
-//       messageA: 'Hello, Vue',
-//       messageB: '<h2>Bye, Vue</h2>',
-//       messageLink: 'https://www.naver.com',
-//     };
-//   },
-//   methods: {
-//     outputRandomMessage() {
-//       const randomNumber = Math.random();
-//       if (randomNumber < 0.5) {
-//         return this.messageA;
-//       } else {
-//         return this.messageB;
-//       }
-//     },
-//   },
-// });
-
-// app.mount('#message-container');
