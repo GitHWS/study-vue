4d712bc1d296b7248d347119f366fef5e6e5a5e1
Hyunwoo Shin <96808980+GitHWS@users.noreply.github.com>
2023-08-13 15:31:00 +0900

    BaseDialog 컴포넌트 transition 애니메이션 적용

--- projects/coach-finder/src/components/ui/BaseDialog.vue
+++ projects/coach-finder/src/components/ui/BaseDialog.vue
@@ -1,21 +1,23 @@
 <template>
   <teleport to="body">
     <div v-if="show" @click="tryClose" class="backdrop"></div>
-    <dialog open v-if="show">
-      <header>
-        <slot name="header">
-          <h2>{{ title }}</h2>
-        </slot>
-      </header>
-      <section>
-        <slot></slot>
-      </section>
-      <menu v-if="!fixed">
-        <slot name="actions">
-          <base-button @click="tryClose">Close</base-button>
-        </slot>
-      </menu>
-    </dialog>
+    <transition name="dialog">
+      <dialog open v-if="show">
+        <header>
+          <slot name="header">
+            <h2>{{ title }}</h2>
+          </slot>
+        </header>
+        <section>
+          <slot></slot>
+        </section>
+        <menu v-if="!fixed">
+          <slot name="actions">
+            <base-button @click="tryClose">Close</base-button>
+          </slot>
+        </menu>
+      </dialog>
+    </transition>
   </teleport>
 </template>
 
@@ -94,6 +96,27 @@ menu {
   display: flex;
   justify-content: flex-end;
   margin: 0;
+}
+
+/* Transition CSS 클래스 */
+.dialog-enter-from,
+.dialog-leave-to {
+  opacity: 0;
+  transform: scale(0.8) translateY(-100px);
+}
+
+.dialog-enter-active {
+  transition: all 0.3s ease-out;
+}
+
+.dialog-leave-active {
+  transition: all 0.3s ease-in translateY();
+}
+
+.dialog-enter-to,
+.dialog-leave-from {
+  opacity: 1;
+  transform: scale(1) translateY(0);
 }
 
 @media (min-width: 768px) {
