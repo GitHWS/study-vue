fad34b254ad6a55f06aae3a952299bb12c2ad155
Hyunwoo Shin <96808980+GitHWS@users.noreply.github.com>
2023-08-13 15:25:22 +0900

    fetch 후 1분 초과 시 타임스탬프 상태에 따른 refetch 및 Refresh 버튼을 이용한 강제 refetch 기능 추가

--- projects/coach-finder/src/store/modules/coaches/actions.js
+++ projects/coach-finder/src/store/modules/coaches/actions.js
@@ -28,7 +28,11 @@ export default {
       id: userId,
     });
   },
-  async loadCoaches(context) {
+  async loadCoaches(context, payload) {
+    if (!payload.forceRefresh && !context.getters.shouldUpdate) {
+      return;
+    }
+
     const response = await fetch(
       `https://vue-http-demo-f4838-default-rtdb.firebaseio.com/coaches.json`
     );
@@ -55,5 +59,6 @@ export default {
     }
 
     context.commit('setCoaches', coaches);
+    context.commit('setFetchTimestamp');
   },
 };
