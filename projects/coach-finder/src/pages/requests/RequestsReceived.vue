992b9a85a86b37c9207b2f4471487d3144889298
Hyunwoo Shin <96808980+GitHWS@users.noreply.github.com>
2023-08-13 14:12:14 +0900

    firebase 데이터베이스로부터 활성 사용자의 requests 데이터를 로드

--- projects/coach-finder/src/pages/requests/RequestsReceived.vue
+++ projects/coach-finder/src/pages/requests/RequestsReceived.vue
@@ -1,10 +1,16 @@
 <template>
+  <base-dialog :show="!!error" title="An Error Occured!" @close="handleError">
+    <p>
+      {{ error }}
+    </p>
+  </base-dialog>
   <section>
     <base-card>
       <header>
         <h2>Request Received</h2>
       </header>
-      <ul v-if="hasRequests">
+      <base-spinner v-if="isLoading"></base-spinner>
+      <ul v-else-if="hasRequests && !isLoading">
         <request-item
           v-for="req in receivedRequests"
           :key="req.id"
@@ -20,10 +26,18 @@
 
 <script>
 import RequestItem from '../../components/requests/RequestItem';
+import BaseDialog from '../../components/ui/BaseDialog.vue';
 
 export default {
   components: {
     RequestItem,
+    BaseDialog,
+  },
+  data() {
+    return {
+      isLoading: false,
+      error: null,
+    };
   },
   computed: {
     receivedRequests() {
@@ -32,6 +46,24 @@ export default {
     },
     hasRequests() {
       return this.$store.getters['requests/hasRequests'];
+    },
+  },
+  created() {
+    this.loadRequests();
+  },
+  methods: {
+    async loadRequests() {
+      this.isLoading = true;
+      try {
+        await this.$store.dispatch('requests/fetchRequest');
+      } catch (error) {
+        this.error = error.message || 'Somethig failed!';
+      }
+
+      this.isLoading = false;
+    },
+    handleError() {
+      this.error = null;
     },
   },
 };
--- projects/coach-finder/src/store/modules/requests/actions.js
+++ projects/coach-finder/src/store/modules/requests/actions.js
@@ -1,11 +1,60 @@
 export default {
-  contactCoach(context, payload) {
+  async contactCoach(context, payload) {
     const newRequest = {
-      id: new Date().toISOString(),
-      coachId: payload.coachId,
       userEmail: payload.email,
       message: payload.message,
     };
+
+    const response = await fetch(
+      `https://vue-http-demo-f4838-default-rtdb.firebaseio.com/requests/${payload.coachId}.json`,
+      {
+        method: 'POST',
+        body: JSON.stringify(newRequest),
+      }
+    );
+
+    const responseData = await response.json();
+
+    if (!response.ok) {
+      const error = new Error(responseData.message || 'Faild to send Request.');
+      throw error;
+    }
+
+    // NOTE fireBase에서 POST 메서드를 통한 데이터 추가 시 name이라는 필드를 고유한 값과 함께 생성하는데 이는 추가하는 데이터의 고유한 id이다.
+    newRequest.id = responseData.name;
+    newRequest.coachId = payload.coachId;
+
     context.commit('addRequest', newRequest);
+  },
+  async fetchRequest(context) {
+    const coachId = context.rootGetters.userId;
+
+    const response = await fetch(
+      `https://vue-http-demo-f4838-default-rtdb.firebaseio.com/requests/${coachId}.json`
+    );
+
+    const responseData = await response.json();
+
+    if (!response.ok) {
+      const error = new Error(
+        responseData.message || 'Failed to fetch request!'
+      );
+      throw error;
+    }
+
+    const requests = [];
+
+    for (const key in responseData) {
+      const request = {
+        id: key,
+        coachId: coachId,
+        userEmail: responseData[key].userEmail,
+        message: responseData[key].message,
+      };
+
+      requests.push(request);
+    }
+
+    context.commit('setRequests', requests);
   },
 };
