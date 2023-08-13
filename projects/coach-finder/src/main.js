df07fa5d5dece130a8c6b5157e2b9fe511a52f83
Hyunwoo Shin <96808980+GitHWS@users.noreply.github.com>
2023-08-13 13:19:59 +0900

    전역 컴포넌트 BaseDialog 추가

--- projects/coach-finder/src/main.js
+++ projects/coach-finder/src/main.js
@@ -8,6 +8,7 @@ import BaseCard from './components/ui/BaseCard';
 import BaseButton from './components/ui/BaseButton';
 import BaseBadge from './components/ui/BaseBadge';
 import BaseSpinner from './components/ui/BaseSpinner.vue';
+import BaseDialog from './components/ui/BaseDialog.vue';
 
 const app = createApp(App);
 
@@ -18,5 +19,6 @@ app.component('base-card', BaseCard);
 app.component('base-button', BaseButton);
 app.component('base-badge', BaseBadge);
 app.component('base-spinner', BaseSpinner);
+app.component('base-dialog', BaseDialog);
 
 app.mount('#app');
